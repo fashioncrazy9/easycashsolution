b3053d0e3b18a3ce78aa951ddf742e6b92059263
Binh <binh171989@gmail.com>
2013-07-11 14:59:25 +0700

    new 3

--- app/js/app.js
+++ app/js/app.js
@@ -2,13 +2,16 @@
 
 /* App Module */
 
-angular.module('ecs', ['ecsFilters']).
+var app = angular.module('ecs', ['ecsFilters']).
   config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
+  	
+  	$routeProvider.  		
   		when('/login', {templateUrl: 'partials/login.html',   controller: LoginController}).
   		when('/books', {templateUrl: 'partials/book.html',   controller: BookController}).
   		when('/test', {templateUrl: 'partials/test.html',   controller: TestController}).
+     	when('/service', {templateUrl: 'partials/testService.html',   controller: HelloCtrl}).
      	when('/phones', {templateUrl: 'partials/phone-list.html',   controller: PhoneListCtrl}).
      	when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
       	otherwise({redirectTo: '/login'});
+    // $locationProvider.html5Mode(true);
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -24,7 +24,9 @@ function PhoneDetailCtrl($scope, $routeParams, $http) {
 var client = new Usergrid.Client({
     orgName:"binhnguyen",
     appName:"magrabbit"
-  });
+});
+
+
 
  // Login controller
 function LoginController($scope) {
@@ -44,7 +46,7 @@ function LoginController($scope) {
         $("#response").html('<pre>'+output+'</pre>');
         client.setToken(client.token);
         //redriect view
-        location.href ="http://localhost:8000/#/test";
+        location.href ="http://localhost:8000/#/books";
       }
     });
   };  
@@ -53,8 +55,12 @@ function LoginController($scope) {
 
 // User controller
 function BookController($scope) {
-     $scope.books =[];
-      $scope.isLoading = false;
+  $scope.books =[];
+  $scope.uuid = "";
+  $scope.bookName ="";
+  $scope.author ="";
+  $scope.bookObject={};
+
   $scope.getBooks = function() {
     var options={
       type:"book",//$('#cboPath').val(),
@@ -62,39 +68,74 @@ function BookController($scope) {
     };
     client.createCollection(options, function(error, data){
        
-        data.fetch();
-           console.log( data._list); 
-         $scope.books = data._list;
-         $scope.$apply($scope.books);    
-           var output = JSON.stringify($scope.books, null, 2);
-        $("#response").html('<pre>'+output+'</pre>');
-
+      data.fetch();
+      console.log( data._list); 
+       $scope.books = data._list;
+       $scope.$apply($scope.books);    
+         var output = JSON.stringify($scope.books, null, 2);
+      $("#response").html('<pre>'+output+'</pre>');
     });
   };  
 
-    $scope.addBook = function(book) {          
-        console.log(book);
-        var options = {
-            type:"book",
-            //qs:{ql:'order by title desc'}
-        };
-
-        client.createEntity(options, function(error, obj){
-            if(error)
-              alert("Error!!!");
-            else{
-             
-              obj.set("name", book.bookName);
-              obj.set("author", book.author);
-              obj.save();
-              //  $('#msg').append("<p>Successful!</p>");
-              // $('#inserted').append("<p>"+obj.get("name")+" / "+obj.get("author")+"</p> ");
-
-              // $('#json').append(JSON.stringify(obj.get()));
-            }
-        });
+  $scope.addBook = function(bookName, author) {          
+    console.log("bookName = " + bookName);
+    console.log("author = " + author);
+    var options = {
+        type:"book",
+        //qs:{ql:'order by title desc'}
     };
 
+    client.createEntity(options, function(error, obj){
+        if(error)
+          alert("Error!!!");
+        else{
+         
+          obj.set("name", bookName);
+          obj.set("author", author);
+          obj.save();
+          //  $('#msg').append("<p>Successful!</p>");
+          // $('#inserted').append("<p>"+obj.get("name")+" / "+obj.get("author")+"</p> ");
+
+          // $('#json').append(JSON.stringify(obj.get()));
+        }
+    });
+  };
+
+  $scope.deleteBook = function(book) {          
+    console.log(book._data.uuid);
+    var options={
+      method: 'DELETE',
+      endpoint:"books/" + book._data.uuid
+    }
+    client.request(options, function(err, data){
+      console.log(data);
+      if(err){ 
+        $("#msg").html('<pre>ERROR: '+data+'</pre>');
+      } else {   
+         $("#msg").html('<pre>'+ book._data.name +' has deleted</pre>');
+      }
+    });
+  }
+
+  $scope.selectedBook = function(book) {          
+    console.log(book._data.uuid);
+    $scope.bookName =book._data.name;
+    $scope.author =book._data.author;
+    $scope.bookObject = book;    
+  }
+
+  $scope.updateBook = function(bookName, author) {          
+    console.log(bookName, author);
+    var obj = $scope.bookObject;
+    // obj.set("name":bookName, "author":author);
+    // obj.save(function (err) {
+    //     if (err) {
+    //       window.location = "#login";
+    //     } else {
+    //       $('#msg').html('<strong>Your account was updated</strong>');
+    //     }
+    //   });    
+  }
    
 } // end Book controller
 
@@ -107,15 +148,25 @@ function TestController($scope) {
 
     $scope.list = [];
     $scope.text = 'hello';
-    $scope.submit = function() {
-        if (this.text) {
-            this.list.push(this.text);
-            this.text = '';
-        }
-  };
+  $scope.submit = function() {
+      if (this.text) {
+          this.list.push(this.text);
+          this.text = '';
+      }
+  }
 
-   $scope.numbers = [1,2,3,4,5,6,7,8,9];   
+  $scope.numbers = [1,2,3,4,5,6,7,8,9];   
   $scope.limit = 3;
+
 
 } // end Test controller
 
+//test for service
+function HelloCtrl($scope, testService, testFactory)
+{
+    $scope.name = "Binh Nguyen";
+    $scope.fromService = testService.sayHello($scope.name);
+    $scope.fromFactory = testFactory.sayHello("World");
+    $scope.fromService1 = testService.sayGoodbye($scope.name);
+    $scope.fromFactory1 = testFactory.sayGoodbye("World");
+}
